fc58e77b6238f47340964e11da4c7835601ceddc
Noel Chacko <69741906+noel-chacko@users.noreply.github.com>
2024-10-24 19:40:54 -0400

    added twitter timeline from temple news to home page

--- exploring-national-parks/src/HomePage.js
+++ exploring-national-parks/src/HomePage.js
@@ -4,22 +4,38 @@
  * @module HomePage
  * @returns {JSX.Element} The rendered home page component.
  */
-import React from 'react'
-import Welcome from './HomePage/Components/Welcome'
-import Buttons from './HomePage/Components/Buttons'
-import yosemite from './HomePage/Assets/yosemite.jpg';
-import './Style/homepage.css'
+import React, { useEffect } from 'react';
+import Welcome from './HomePage/Components/Welcome';
+import Buttons from './HomePage/Components/Buttons';
 import HighlightGallery from './HomePage/Components/HighlightGallery';
-const HomePage = () => {
-  return (
-    // <Navbar/>
-    <div className = "home-page main-component">
-        {/* <h1>Test Hello</h1> */}
-        <Welcome/>
-        <HighlightGallery/>
-        <Buttons/>
-    </div>
-  )
-}
+import './Style/homepage.css';
 
-export default HomePage
+const HomePage = () => {
+  // Add useEffect to load Twitter script
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div className="home-page main-component">
+      <Welcome />
+      <HighlightGallery />
+      <Buttons />
+      
+      {/* Twitter Timeline Embed */}
+      <div style={{ marginTop: '20px' }}>
+        <a
+          className="twitter-timeline"
+          href="https://twitter.com/TheTempleNews?ref_src=twsrc%5Etfw"
+        >
+          Tweets by TheTempleNews
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
